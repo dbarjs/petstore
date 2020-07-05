14f48581edfeef15e48a57804825c7e99450910c
Eduardo Barros <joseeduardobarros@gmail.com>
2020-07-05 01:35:40 -0300

    Create Product Model

---
+++ packages/server/src/models/Product.ts
@@ -0,0 +1,36 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('products')
+class Product {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  category: string;
+
+  @Column('decimal')
+  price: number;
+
+  @Column('decimal')
+  quantity: number;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default Product;
