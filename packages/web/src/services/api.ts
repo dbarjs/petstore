58cc9352273de62cb381535814c4fbdd01462edf
Eduardo Barros <joseeduardobarros@gmail.com>
2020-07-07 19:59:04 -0300

    Add env log

--- packages/web/src/services/api.ts
+++ packages/web/src/services/api.ts
@@ -2,6 +2,8 @@ import axios from 'axios';
 
 require('dotenv').config();
 
+console.log(process.env);
+
 const api = axios.create({
   baseURL: process.env.SERVER_HOST || 'http://localhost:3333',
 });
