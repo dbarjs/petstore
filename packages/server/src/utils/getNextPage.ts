b9d8c197a81213751b5c0929b290e3e219f1879f
Eduardo Barros <joseeduardobarros@gmail.com>
2020-07-07 18:47:27 -0300

    Fix pagination bug

--- packages/server/src/utils/getNextPage.ts
+++ packages/server/src/utils/getNextPage.ts
@@ -5,7 +5,7 @@ export default function getNextPage(
   skip: number,
 ): number {
   const hasMorePages = total > skip + take;
-  const currentPage = skip / take;
+  const currentPage = skip > 0 ? skip / take : 1;
   const nextPage = currentPage + 1;
 
   return hasMorePages ? nextPage : 0;
