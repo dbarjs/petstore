f17edf6390a19ba2978e8cbe7febe3446bdd715c
Eduardo Barros <joseeduardobarros@gmail.com>
2020-07-08 08:00:42 -0300

    Add default configs

--- packages/server/ormconfig.js
+++ packages/server/ormconfig.js
@@ -1,15 +1,19 @@
 require('dotenv').config();
 
+const port = process.env.DB_PORT || 5432;
 const host = process.env.DB_HOST || 'localhost';
+const username = process.env.DB_USER || 'postgres';
+const password = process.env.DB_PASS || 'docker';
+const database = process.env.DB_NAME || 'petstore';
 
 module.exports = [
   {
     type: 'postgres',
     host,
-    port: process.env.DB_PORT,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
+    port,
+    username,
+    password,
+    database,
     entities: ['./src/models/*.ts'],
     migrations: ['./src/database/migrations/*.ts'],
     cli: {
@@ -20,10 +24,10 @@ module.exports = [
     name: 'seed',
     type: 'postgres',
     host,
-    port: process.env.DB_PORT,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
+    port,
+    username,
+    password,
+    database,
     entities: ['./src/models/*.ts'],
     migrations: ['./src/database/seeds/*.ts'],
     cli: {
